ca64bd750c590f4f987bf5a993d988272cf46f1a
Ondrej Vana <vana.ondrej@gmail.com>
2021-01-20 21:41:37 -0800

    feat: Add "cancelAction" color

--- src/components/SignOutButton.js
+++ src/components/SignOutButton.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { Button } from 'react-native'
 import Authentication from '../Authentication'
+import Colors from '../Colors'
 import { StoreContext } from '../StoreContext'
 
 export default class SignOutButton extends Component {
   render() {
     let AppStore = this.context.application
     return (
-      <Button title='Sign out' onPress={async () => {
+      <Button title='Sign out' color={ Colors.cancelAction } onPress={async () => {
         let auth = new Authentication()
         let result = await auth.logout()
         if (!result) {
