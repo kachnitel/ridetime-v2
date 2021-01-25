c8667df0c53657114d8cba50ae36ddccc81f8b66
Ondrej Vana <vana.ondrej@gmail.com>
2021-01-24 19:56:52 -0800

    feat: UserItem

---
+++ src/components/UserList.js
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { heightPercentageToDP, widthPercentageToDP } from 'react-native-responsive-screen'
+import { User } from '../stores/UserStore'
+import ProfilePicture from './ProfilePicture'
+import TrailDifficultyIcon from './TrailDifficultyIcon'
+
+export const UserItem = ({ user }) => <View style={ styles.container }>
+  <View>
+    <ProfilePicture uri={ user.picture } style={ styles.picture } />
+    { user.level && <TrailDifficultyIcon
+      level={ user.level }
+      style={ styles.levelIcon }
+      size={ heightPercentageToDP(5) }
+    /> }
+  </View>
+  <View style={ styles.detailsContainer }>
+    <Text style={ styles.name }>{ user.name }</Text>
+    <Text style={ styles.hometown }>{ user.hometown }</Text>
+  </View>
+</View>
+
+UserItem.propTypes = {
+  user: PropTypes.instanceOf(User)
+}
+
+// export default UserList
+
+const styles = StyleSheet.create({
+  container: {
+    height: heightPercentageToDP(15),
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  picture: {
+    height: '80%'
+  },
+  name: {
+    fontSize: heightPercentageToDP(4)
+  },
+  hometown: {
+
+  },
+  levelIcon: {
+    position: 'absolute',
+    bottom: '2.5%',
+    right: '2.5%'
+  },
+  detailsContainer: {
+    padding: widthPercentageToDP(2)
+  }
+})
--- src/navigation/screens/HomeScreen.js
+++ src/navigation/screens/HomeScreen.js
@@ -1,31 +1,30 @@
 import { Observer } from 'mobx-react'
 import React from 'react'
+import { useContext } from 'react'
 import { Text, View, StyleSheet, Button } from 'react-native'
+import { UserItem } from '../../components/UserList'
 import { StoreContext } from '../../StoreContext'
 
-export default class HomeScreen extends React.Component {
-  render () {
-    return (
-      <View style={styles.container}>
-        <Text> Home </Text>
-        <Observer>
-          {() => <Text>{ this.context.user.currentUser.name }</Text>}
-        </Observer>
-        <Button title='Log user store' onPress={ () => {
-          console.log(this.context.user.users)
-        }} />
-      </View>
-    )
-  }
+const HomeScreen = () => {
+  let { user: UserStore } = useContext(StoreContext)
+  return <View style={styles.container}>
+    <Text> Home </Text>
+    <Observer>
+      {() => <UserItem user={UserStore.currentUser} />}
+    </Observer>
+    <Button title='Log user store' onPress={ () => {
+      console.log(UserStore.users)
+    }} />
+  </View>
 }
 
-HomeScreen.contextType = StoreContext
 
+export default HomeScreen
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 })
